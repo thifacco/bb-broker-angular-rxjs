297d26c3679a49f86c2cd6115ffdca56700aa0f4
Tiago <tiago@gobacklog.com>
2022-10-20 11:42:55 -0300

    Push

---
+++ README.md
@@ -0,0 +1,18 @@
+# Byte Bank Broker
+Home broker desenvolvivo com Angular + RxJs
+
+## Instalar dependências
+
+API
+```
+cd ./api
+npm install
+npm start
+```
+
+APP
+```
+cd ./byte-bank-broker
+npm install
+npm start
+```
---
+++ byte-bank-broker/src/app/acoes/acoes.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { Acoes } from './modelo/acoes';
+import { AcoesService } from './acoes.service';
+
+@Component({
+  selector: 'app-acoes',
+  templateUrl: './acoes.component.html',
+  styleUrls: ['./acoes.component.css'],
+})
+export class AcoesComponent implements OnInit {
+  acoesInput = new FormControl();
+  acoes: Acoes;
+
+  constructor(
+    private acoesService: AcoesService
+  ) {}
+
+  ngOnInit(): void {
+    this.acoesService.getAcoes().subscribe(data => {
+      this.acoes = data.payload;
+    });
+  }
+}
