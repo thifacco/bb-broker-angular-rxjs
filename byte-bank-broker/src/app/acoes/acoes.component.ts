884b8d87eec67a61c77b8b63094844b2beff0d1e
Tiago <tiago@gobacklog.com>
2022-10-21 16:00:07 -0300

    Pipe async

--- byte-bank-broker/src/app/acoes/acoes.component.ts
+++ byte-bank-broker/src/app/acoes/acoes.component.ts
@@ -1,24 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { Acoes } from './modelo/acoes';
 import { AcoesService } from './acoes.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-acoes',
   templateUrl: './acoes.component.html',
   styleUrls: ['./acoes.component.css'],
 })
-export class AcoesComponent implements OnInit {
+export class AcoesComponent implements OnInit, OnDestroy {
   acoesInput = new FormControl();
-  acoes: Acoes;
-
+  
   constructor(
     private acoesService: AcoesService
   ) {}
 
+  
+  // Exemplo 1: 
+  // Utilizando subscription para criar/destruir o observable
+  acoes: Acoes;
+  private subscription: Subscription
   ngOnInit(): void {
-    this.acoesService.getAcoes().subscribe(data => {
+    this.subscription = this.acoesService.getAcoes().subscribe(data => {
       this.acoes = data;
     });
   }
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
+
+
+  // Exemplo 2: 
+  // Utilizando o pipe async no template
+  acoes$ = this.acoesService.getAcoes();
 }
